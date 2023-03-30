b0f6cf3f05dee677182e4865f8abbf454e7c1f96
Muhammad Nur Fadli <muhammad.fadli@lemonilo.com>
2023-03-30 11:38:26 +0700

    add logrus

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`../config/config.json`)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool(`debug`) {
+		fmt.Println("Service RUN on DEBUG mode")
+	}
+}
+
+func main() {
+
+}
